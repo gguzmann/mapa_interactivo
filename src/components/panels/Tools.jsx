a0145541825ed5aa8e7354d3022e30957ad0fcf4
gabusho92 <g.guzman.nunez@gmail.com>
2023-01-10 11:10:47 -0300

    disabled buttom tools

--- src/components/panels/Tools.jsx
+++ src/components/panels/Tools.jsx
@@ -3,7 +3,7 @@ import { storeContext } from '../../context/storeContext'
 
 export const Tools = () => {
 
-  const {tool, setTool, current, setCurrent} = useContext(storeContext)
+  const { tool, setTool, current, setCurrent } = useContext(storeContext)
 
   return (
     <div className='panel_tools bg-light shadow'>
@@ -17,14 +17,14 @@ export const Tools = () => {
         <button className={`btn btn-outline-primary w-75 d-flex justify-content-center ${tool == 'location' && 'active'}`} onClick={() => setTool('location')}><i className="fa-solid fa-location-dot"></i></button>
       </div>
       <div className='d-flex justify-content-center mt-1'>
-        <button className={`btn btn-outline-primary w-75 d-flex justify-content-center  ${tool == 'layer' && 'active'}`} onClick={() => setTool('layer')}><i className="fa-solid fa-layer-group"></i></button>
+        <label style={{ color: current.color }} className='btn btn-outline-primary w-75 d-flex justify-content-center' htmlFor="colorTool"><i className="fa-solid fa-droplet"></i></label>
+        <input type="color" name="colorTool" id="colorTool" value={'#bf1f0d'} onChange={(e) => setCurrent({ ...current, color: e.target.value })} hidden />
       </div>
       <div className='d-flex justify-content-center mt-1'>
-        <button className={`btn btn-outline-primary w-75 d-flex justify-content-center ${tool == 'initialPos' && 'active'}`} onClick={() => setTool('initialPos')}><i className="fa-solid fa-street-view"></i></button>
+        <button className={`btn btn-outline-primary w-75 d-flex justify-content-center  ${tool == 'layer' && 'active'}`} onClick={() => setTool('layer')} disabled><i className="fa-solid fa-layer-group"></i></button>
       </div>
       <div className='d-flex justify-content-center mt-1'>
-        <label style={{color: current.color}} className='btn btn-outline-primary w-75 d-flex justify-content-center' htmlFor="colorTool"><i className="fa-solid fa-droplet"></i></label>
-        <input type="color" name="colorTool" id="colorTool" value={'#bf1f0d'} onChange={(e) => setCurrent({...current, color: e.target.value})} hidden/>
+        <button className={`btn btn-outline-primary w-75 d-flex justify-content-center ${tool == 'initialPos' && 'active'}`} onClick={() => setTool('initialPos')} disabled><i className="fa-solid fa-street-view"></i></button>
       </div>
     </div>
   )
